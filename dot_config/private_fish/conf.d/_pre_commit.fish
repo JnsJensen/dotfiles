4a6ba7bc7ec902eaa8983314bafdd02d98b834ab
Jens <jens.jens@live.com>
2023-04-27 23:41:21 +0200

    feat: fish

---
+++ dot_config/private_fish/conf.d/_pre_commit.fish
@@ -0,0 +1,64 @@
+status is-interactive; or return
+
+if not command --query pre-commit
+    _git_fish_echo "pre-commit not installed. no hooks will be enabled."
+    return
+end
+
+set -g _git_fish_git_directories_visisted
+
+function _git_fish_check_for_pre_commit --on-event in_git_repo_root_directory
+    contains -- $PWD $_git_fish_git_directories_visisted; and return
+
+    # used to highlight the .pre-commit-config.yaml file when it is printed
+    set -l dot_pre_commit_config_yaml (set_color --bold)".pre-commit-config.yaml"(set_color normal)
+    # When running on A Fedora Silverblue system, the PWD is /var/home/username
+    set -l cwd (string replace --regex "^/var$HOME" "~" -- $PWD | string replace --regex "^$HOME" "~")
+
+    if test -f .pre-commit-config.yaml
+        _git_fish_echo "a $dot_pre_commit_config_yaml file was found in $(set_color --bold)$cwd$(set_color normal)"
+        if command --query bat
+            command bat --plain .pre-commit-config.yaml
+        else
+            command cat .pre-commit-config.yaml
+        end
+        # check if hooks are installed
+        if not test -f ./.git/hooks/pre-commit
+            _git_fish_echo "pre-commit hooks not installed. installing..."
+            command pre-commit install --install-hooks 2>/dev/null
+            _git_fish_echo "pre-commit hooks installed."
+        else
+            _git_fish_echo "pre-commit hooks installed."
+            set -l auto_update_command (echo -n "pre-commit autoupdate" | fish_indent --ansi)
+            _git_fish_echo "to autoupdate them, run: $auto_update_command"
+        end
+    else
+        _git_fish_echo "no $dot_pre_commit_config_yaml file found in $(set_color --bold)$cwd$(set_color normal)."
+        if set --query GIT_FISH_PRE_COMMIT_AUTO_INSTALL
+            _git_fish_echo "GIT_FISH_PRE_COMMIT_AUTO_INSTALL is set. generating a sample $dot_pre_commit_config_yaml file and installing it..."
+            command pre-commit sample-config | tee .pre-commit-config.yaml && command pre-commit install --install-hooks
+            _git_fish_echo "pre-commit hooks installed."
+        else
+            set -l generate_sample_config_command "pre-commit sample-config | tee .pre-commit-config.yaml && pre-commit install"
+            _git_fish_echo "a sample $dot_pre_commit_config_yaml file can be generated and installed with:"
+            echo -en "\t"
+            echo "$generate_sample_config_command" | fish_indent --ansi
+            set -l abbreviation pcg
+            if not abbr --query $abbreviation
+                _git_fish_echo "adding abbreviation: $(set_color --reverse)$abbreviation$(set_color normal) for"
+                echo -en "\t"
+                echo "$generate_sample_config_command" | fish_indent --ansi
+                abbr --add $abbreviation "$generate_sample_config_command"
+            end
+            _git_fish_echo "the abbreviation $(set_color --reverse)$abbreviation$(set_color normal) can be used to generate a sample $dot_pre_commit_config_yaml file."
+        end
+    end
+
+    # check if we've already visited this directory
+    # if not, add it to the list of visited directories
+    # this is to prevent annoying the user by asking them again and again in
+    # the same shell session.
+    if not contains -- $PWD $_git_fish_git_directories_visisted
+        set --append _git_fish_git_directories_visisted $PWD
+    end
+end
--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,10 +3,9 @@ if status is-interactive
     atuin init fish | source
 end
 
-function list_dir --on-variable PWD; ls -GF; end;
-
 abbr --set-cursor -a gc 'git commit -m "%"'
-abbr -a gca 'git commit -am "%"'
+abbr -a ga 'git add .'
+abbr --set-cursor -a gca 'git commit -am "%"'
 abbr -a gs 'git status'
 abbr -a gsw 'git switch'
 abbr -a gswc 'git switch -c'
@@ -25,10 +24,19 @@ abbr -a db 'distrobox'
 abbr -a dbc 'distrobox create'
 abbr -a dbl 'distrobox list'
 abbr -a dbe 'distrobox enter'
+abbr -a dbs 'distrobox stop'
+abbr -a dbr 'distrobox rm'
 
 abbr -a pm 'podman'
 
 abbr -a cm 'chezmoi'
+
+abbr -a ts 'tailscale'
+abbr -a tss 'tailscale status'
+abbr -a tsip 'tailscale ip'
+abbr -a tsup 'tailscale up'
+abbr -a tsdown 'tailscale down'
+abbr -a tssh 'tailscale ssh'
 
 alias ls 'exa --icons --group-directories-first --classify --grid'
 alias ll 'exa --icons --group-directories-first --classify --long --header --git'
@@ -38,18 +46,44 @@ alias lst 'exa --icons --group-directories-first --classify --tree --level=2'
 alias llt 'exa --icons --group-directories-first --classify --tree --level=2 --long --header --git'
 alias llet 'exa --icons --group-directories-first --classify --tree --level=2 --long --header --git --extended'
 
-alias contiker 'podman run --privileged --sysctl net.ipv6.conf.all.disable_ipv6=0 --mount type=bind,source=/home/jens/repos/contiki-ng,destination=/home/user/contiki-ng -e DISPLAY=:0 -v/tmp/.X11-unix:/tmp/.X11-unix -v /dev/bus/usb:/dev/bus/usb -v /dev/ttyUSB0:/dev/ttyUSB0:rw --user root:root --userns keep-id --user 1000  -ti contiker/contiki-ng'
+# alias contiker 'podman run --privileged --sysctl net.ipv6.conf.all.disable_ipv6=0 --mount type=bind,source=/home/jens/repos/contiki-ng,destination=/home/user/contiki-ng -e DISPLAY=:0 -v/tmp/.X11-unix:/tmp/.X11-unix -v /dev/bus/usb:/dev/bus/usb -v /dev/ttyUSB0:/dev/ttyUSB0:rw --user root:root --userns keep-id --user 1000  -ti contiker/contiki-ng'
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if not test -f /run/.containerenv 
-    eval /home/jens/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-end
-# <<< conda initialize <<<
-
-if set -q ZELLIJ
-else
-  zellij
-end
+# # >>> conda initialize >>>
+# # !! Contents within this block are managed by 'conda init' !!
+# if not test -f /run/.containerenv 
+#     eval /home/jens/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# end
+# # <<< conda initialize <<<
 
 set -x EDITOR /usr/bin/micro
+
+
+function list_dir --on-variable PWD
+  ls -GF
+end
+
+sleep 0.1
+
+# if set -q ZELLIJ
+   # conda activate base
+   # conda deactivate
+# else
+   # zellij -l orin
+# end
+
+alias vim 'nvim'
+if test -z "$TMUX" 
+  if tmux ls &> /dev/null
+    # set SESSION_AMOUNT (tmux ls | wc -l)
+    # if test (echo $SESSION_AMOUNT) -gt 1
+      # tmux ls
+      # read -P 'Choose session to attach to: ' CHOSEN_SESSION
+      # tmux attach-session -t $CHOSEN_SESSION
+    # end
+    echo 'Attaching to last session'
+    tmux a
+  else
+    echo 'No tmux sessions, creating a new one'
+    tmux
+  end
+end
